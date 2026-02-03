d03b3f8db609d89db732cb03a8566b6d598887a4
anton <satsuk.anton2@gmail.com>
2026-02-03 18:28:32 +0100

    feat: second player controls, new character, architecture improvements

--- fighter.py
+++ fighter.py
@@ -2,17 +2,63 @@ import pygame
 
 
 class Fighter():
-    def __init__(self, x, y):
+    def __init__(self, x, y, data, sprite_sheet, animation_steps):
+        self.size = data[0]
+        self.image_scale = data[1]
+        self.offset = data[2]
         self.flip = False
+        self.animation_list = self.load_images(sprite_sheet,
+                                               animation_steps)  # Load in the sheet straight away. List of lists of animations
+        self.action = 0  # 0 - IDLE 1 - Walk
+        self.frame_index = 0
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.update_time = pygame.time.get_ticks()
         self.rect = pygame.Rect(x, y, 200, 200)
         self.vel_y = 0
+        self.running = False
+        self.jumping = False
+        self.attacking = False
+        self.attack_type = 0
+        self.health = 100
+        self.hit = False
+        self.death = False
+        self.walk_sound = pygame.mixer.Sound(
+            "sfx/knight-right-footstep-on-gravel-4-with-chainmail-101937.mp3"
+        )
+        self.sword_attack1_sound = pygame.mixer.Sound("sfx/sword_sfx/sword-slice-393847.mp3")
+        self.sword_attack2_sound = pygame.mixer.Sound("sfx/sword_sfx/sword-slashing-game-sound-effect-1-379228.mp3")
+        self.sword_attack3_sound = pygame.mixer.Sound("sfx/sword_sfx/short-fire-whoosh_1-317280.mp3")
+        self.sword_attack4_sound = pygame.mixer.Sound("sfx/sword_sfx/fire-breath-6922.mp3")
+        self.orc_attack_sound = pygame.mixer.Sound("sfx/sword_sfx/character-falling-on-ground-250069.mp3")
+        self.walk_sound.set_volume(0.2)
+        self.sword_attack1_sound.set_volume(0.4)
+        self.sword_attack2_sound.set_volume(0.2)
+        self.sword_attack3_sound.set_volume(0.15)
+        self.walk_sound_playing = False
+        self.attack_sound_played = False
 
-    def move(self, SCREEN_WIDTH, SCREEN_HEIGHT):
-        SPEED = 10
+    def load_images(self, sprite_sheet, animation_steps):
+        # extract images from sprite sheet
+        animation_list = []  # A list that contains lists of rows from a sprite sheet
+        for y, animation in enumerate(animation_steps):
+            temp_img_list = []
+            for x in range(animation):
+                temp_img = sprite_sheet.subsurface(x * self.size, y * self.size, self.size, self.size)
+                temp_img_list.append(
+                    pygame.transform.scale(temp_img, (self.size * self.image_scale, self.size * self.image_scale)))
+            animation_list.append(temp_img_list)
+            # print(animation_list)
+
+        return animation_list
+
+    def move(self, SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER):
+        SPEED = 6
         GRAVITY = 2
         dx = 0
         dy = 0
         FLOOR_HEIGHT = 100
+        self.running = False
+        self.flip = False
 
         # Key presses
         key = pygame.key.get_pressed()
@@ -21,23 +67,93 @@ class Fighter():
         # SPLIT IN TWO CLASS INSTANCES
         #
         # MOVEMENT BINDS WASD
-        if key[pygame.K_d]:  # RIGHT
-            dx += SPEED
-        if key[pygame.K_a]:  # LEFT
-            dx -= SPEED
-        # jumping
-        if key[pygame.K_w]:  # UP
-            self.vel_y -= SPEED
+        if PLAYER == 0:
+            if key[pygame.K_d]:  # RIGHT
+                dx += SPEED
+                self.running = True
+            if key[pygame.K_a]:  # LEFT
+                dx -= SPEED
+                self.running = True
 
-        # MOVEMENT BINDS ARROWS
-        if key[pygame.K_RIGHT]:  # RIGHT
-            dx += SPEED
-        if key[pygame.K_LEFT]:  # LEFT
-            dx -= SPEED
         # jumping
-        if key[pygame.K_UP]:  # UP
-            self.vel_y -= SPEED
-        #
+        if PLAYER == 0:
+            if key[pygame.K_w]:  # UP
+                self.vel_y -= SPEED
+        if PLAYER == 1:
+            if key[pygame.K_SPACE]:  # UP
+                self.vel_y -= SPEED
+
+        if key[pygame.K_i]:  # FLIP TEST
+            if self.flip == True:
+                self.flip = False
+        # MOVEMENT BINDS ARROWS
+        if PLAYER == 1:
+            if key[pygame.K_RIGHT]:  # RIGHT
+                dx += SPEED
+                self.running = True
+            if key[pygame.K_LEFT]:  # LEFT
+                dx -= SPEED
+                self.running = True
+            # jumping
+            if key[pygame.K_UP]:  # UP
+                self.vel_y -= SPEED
+
+        # Attack binds
+        if PLAYER == 0:
+            if not self.attacking:
+                if key[pygame.K_r]:
+                    self.attacking = True
+                    self.attack_type = 1
+                    self.attack_sound_played = False
+                elif key[pygame.K_f]:
+                    self.attacking = True
+                    self.attack_type = 2
+                    self.attack_sound_played = False
+                elif key[pygame.K_v]:
+                    self.attacking = True
+                    self.attack_type = 3
+                    self.attack_sound_played = False
+
+        # Attack binds
+        if PLAYER == 1:
+            if not self.attacking:
+                if key[pygame.K_COMMA]:
+                    self.attacking = True
+                    self.attack_type = 1
+                    self.attack_sound_played = False
+                elif key[pygame.K_PERIOD]:
+                    self.attacking = True
+                    self.attack_type = 2
+                    self.attack_sound_played = False
+                elif key[pygame.K_SLASH]:
+                    self.attacking = True
+                    self.attack_type = 3
+                    self.attack_sound_played = False
+
+        if PLAYER == 1:
+            self.flip = True
+
+        # SFX
+        # walking sound
+        if self.running:
+            if not self.walk_sound_playing:
+                self.walk_sound.play(-2)  # loop
+                self.walk_sound_playing = True
+        else:
+            if self.walk_sound_playing:
+                self.walk_sound.stop()
+                self.walk_sound_playing = False
+        # sword sound
+        if self.attacking and not self.attack_sound_played:
+            if self.attack_type == 1:
+                self.sword_attack1_sound.play()
+            elif self.attack_type == 2:
+                self.sword_attack2_sound.play()
+            elif self.attack_type == 3:
+                self.sword_attack3_sound.play()
+            elif self.attack_type == 4:
+                self.orc_attack_sound.play()
+            self.attack_sound_played = True
 
         # gravity
         self.vel_y += GRAVITY
@@ -60,9 +176,46 @@ class Fighter():
         self.rect.x += dx
         self.rect.y += dy
 
-    def draw(self, surface):
-        pygame.draw.rect(surface, (222, 110, 0), self.rect)
+    # animation loop
+    def update(self):
+        animation_cooldown = 110  # change image frame every 110ms
+        if self.attacking:
+            if self.attack_type == 1:
+                self.update_action(2)  # attack type 1
+            elif self.attack_type == 2:
+                self.update_action(3)  # attack type 2
+            elif self.attack_type == 3:
+                self.update_action(4)  # attack type 3
 
+        elif self.running == True:
+            self.update_action(1)  # walk
+        else:
+            self.update_action(0)  # idle
+
+        self.image = self.animation_list[self.action][self.frame_index]
+        # check if enough time has passed since the last update
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.frame_index += 1
+            self.update_time = pygame.time.get_ticks()
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
+            if self.attacking:
+                self.attacking = False
+                self.attack_sound_played = False
+
+    def draw(self, surface):
+        img = pygame.transform.flip(self.image, self.flip, False)
+        # pygame.draw.rect(surface, (222, 110, 0), self.rect)
+        surface.blit(img,
+                     (self.rect.x - self.offset[0] * self.image_scale, self.rect.y - self.offset[1] * self.image_scale))
+
+    def update_action(self, new_action):
+        # check if the new action is different to the previous one
+        if new_action != self.action:
+            self.action = new_action
+            # update the current animation
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
 #     # Player 1 crouch (S)
 #     if keys[pygame.K_s]:
 #         bottom = player1_rect.bottom
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ SCREEN_WIDTH = 1520
 SCREEN_HEIGHT = 820
 FPS = 60
 display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Medieval Mayhem")
+pygame.display.set_caption("Skjoldd")
 
 # Load background image
 background = pygame.image.load("forest.jpg").convert()  # BG for TEST_ONLY
@@ -17,6 +17,12 @@ background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # Define colors
 GREEN = (26, 66, 28)
+
+# define knight variables - frame size
+KNIGHT_SIZE = 100
+KNIGHT_SCALE = 10  # DO NOT INCREASE ABOVE 100
+KNIGHT_OFFSET = [40, 37]
+KNIGHT_DATA = [KNIGHT_SIZE, KNIGHT_SCALE, KNIGHT_OFFSET]  # 0 - size, 1 - scale, 2 - offset
 
 # Clock that controls FPS
 clock = pygame.time.Clock()
@@ -43,8 +49,46 @@ knight = pygame.image.load(
     "Tiny RPG Character Asset Pack v1.03 -Full 20 Characters/Characters(100x100)/Knight/Knight/Knight.png").convert_alpha()  # load the png sheet
 # Define amount of animation steps
 KNIGHT_ANIMATION_STEPS = [6, 8, 7, 10, 11, 4, 4, 4]  # x,y,z <-> frames per row in sheet
+ARM_ORC_ANIMATION_STEPS = [6, 8, 7, 8, 9, 4, 4, 4]
+knight_y = FLOOR_Y - PLAYER_HEIGHT
 # assign knight_test to be part of class Fighter
-knight_test = Fighter(PLAYER_WIDTH, FLOOR_Y - PLAYER_HEIGHT)
+knight_test = Fighter(PLAYER_WIDTH, knight_y, KNIGHT_DATA, knight, KNIGHT_ANIMATION_STEPS)
+
+# knight2 = pygame.image.load(
+#     "Tiny RPG Character Asset Pack v1.03 -Full 20 Characters/Characters(100x100)/Knight/Knight/Knight.png").convert_alpha()  # load the png sheet
+knight2 = pygame.image.load(
+    "Tiny RPG Character Asset Pack v1.03 -Full 20 Characters/Characters(100x100)/Armored Orc/Armored Orc/Armored Orc.png").convert_alpha()
+
+knight2_test = Fighter(SCREEN_WIDTH - PLAYER_WIDTH * 2, 10, KNIGHT_DATA, knight2, ARM_ORC_ANIMATION_STEPS)
+
+# PLAYER_1 = {
+#     "left": pygame.K_a,
+#     "right": pygame.K_d,
+#     "jump": pygame.K_w,
+#     "attack1": pygame.K_r,
+#     "attack2": pygame.K_f,
+#     "attack3": pygame.K_v
+# }
+#
+# PLAYER_2 = {
+#     "left": pygame.K_LEFT,
+#     "right": pygame.K_RIGHT,
+#     "jump": pygame.K_UP,
+#     "attack1": pygame.K_COMMA,
+#     "attack2": pygame.K_PERIOD,
+#     "attack3": pygame.K_SLASH
+# }
+
+PLAYER_1 = 0
+PLAYER_2 = 1
+background_music = pygame.mixer.Sound("sfx/Royalty Free Epic Celtic Fantasy Music - Highland Song.mp3")
+background_music.set_volume(0.2)
+background_music.play()
+
+forest_sfx = pygame.mixer.Sound("sfx/forest-ambience-296528.mp3")
+forest_sfx.set_volume(0.1)
+forest_sfx.play()
+
 # Game loop
 running = True
 while running:
@@ -64,9 +108,15 @@ while running:
 
     # GAME LOGIC
     # Update knight movement and state
-    knight_test.move(SCREEN_WIDTH, SCREEN_HEIGHT)
+    knight_test.move(SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_1)
+    knight2_test.move(SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_2)
+
+    # Update knight
+    knight_test.update()
+    knight2_test.update()
     # Draw knight sprite
     knight_test.draw(display_surface)
+    knight2_test.draw(display_surface)
     # Present the rendered frame
     pygame.display.update()
 
